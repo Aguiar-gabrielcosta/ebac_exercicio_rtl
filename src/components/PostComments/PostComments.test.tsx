892194f8ef351ad6702709957d6b4afb03b5c70b
Gabriel Aguiar <Aguiar.gc@hotmail.com>
2024-05-13 17:29:06 -0300

    Adiciona o teste de inserção de 2 comentários

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,27 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
 import PostComment from '.';
 
 describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
+    test('Deve renderizar o componente corretamente', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
     });
+
+    test('Deve inserir dois comentários ("Bacana" e "Legal")', () => {
+        render(<PostComment />)
+        fireEvent.change(screen.getByTestId('area-comentario'), {
+            target: {
+                value: 'Bacana'
+            }
+        })
+        fireEvent.click(screen.getByTestId('btn-comentar'))
+        fireEvent.change(screen.getByTestId('area-comentario'), {
+            target: {
+                value: 'Legal'
+            }
+        })
+        fireEvent.click(screen.getByTestId('btn-comentar'))
+        expect(screen.getByText('Bacana')).toBeInTheDocument()
+        expect(screen.getByText('Legal')).toBeInTheDocument()
+    })
 });
